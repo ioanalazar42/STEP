5991e419e9b13fd32182e469fa25b0134d840a64
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-15 14:21:14 +0000

    Add player to game and fix canvas full screen display
    * Add component to the game which represent the player
    * Fix problem with how canvas is rendered on full screen
      ** Implement full screen display of canvas in JS instead of CSS

--- portfolio/src/main/webapp/game_script.js
+++ portfolio/src/main/webapp/game_script.js
@@ -1,11 +1,29 @@
-function startGame() {
-  gameArea.start();
-}
-
 var gameArea = {
   canvas : document.createElement("canvas"),
   start : function() {
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
   }
+}
+
+var player;
+
+function startGame() {
+  gameArea.start();
+  player = new component(30, 30, "red", 10, window.innerHeight/2);
+}
+
+/* Construct a component with specifiec width
+   height, color and coordinates where it is
+   places in the canvas */
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
 }
