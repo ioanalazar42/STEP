cdacd7f65aa9e7a5cdeae5d40550670170fc18bb
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-16 11:35:49 +0000

    Add player controls to game
    * Enable player to move up and down using the keys "w" and "s" respectively

--- portfolio/src/main/webapp/game_script.js
+++ portfolio/src/main/webapp/game_script.js
@@ -5,10 +5,19 @@ var gameArea = {
     this.canvas.height = window.innerHeight;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
 
 var player;
+
+document.addEventListener('keydown', function(event) {
+    console.log(event.key);
+    movePlayer(event.key);
+});
 
 function startGame() {
   gameArea.start();
@@ -21,9 +30,42 @@ function startGame() {
 function component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
+    this.speedX = 1;
+    this.speedY = y;
     this.x = x;
     this.y = y;
-    ctx = gameArea.context;
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.update = function() {
+      ctx = gameArea.context;
+      ctx.fillStyle = color;
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y = this.speedY;
+    }
+}
+
+function updateGameArea() {
+  gameArea.clear();
+  player.newPos();
+  player.update();
+}
+
+function moveUp() {
+     player.speedY -= 20;
+}
+
+function moveDown() {
+     player.speedY += 20;
+}
+
+function movePlayer(key) {
+    switch(key) {
+        case "w":
+          moveUp();
+          break;
+        case "s":
+          moveDown();
+          break;
+    } // switch
 }
