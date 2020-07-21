3737e78ad9f68e3a0e6c477d6e3415d066c2b339
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-21 07:53:41 +0000

    Improve design of the components in the game
    * Create two separate constructors for text and box components with their respective functions and attributes

--- portfolio/src/main/webapp/game_script.js
+++ portfolio/src/main/webapp/game_script.js
@@ -7,7 +7,7 @@ var gameArea = {
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 20);
-    gameArea.mouseControl = false;
+    this.mouseControl = false;
     window.addEventListener('keydown', function (e) {
       gameArea.key = e.keyCode;
     })
@@ -58,94 +58,100 @@ const keyActions = {
 }
 
 function startGame() {
-  player = new component(30, 30, "red", 10, window.innerHeight/2);
-  score = new component("30px", "Consolas", "black", window.innerWidth - 200, 30, "text");
-
-  // create a component for each instruction box
-  InstrUp = new component("20px", "Consolas", "black", window.innerWidth - 250, window.innerHeight - 90, "text");
-  InstrDown = new component("20px", "Consolas", "black", window.innerWidth - 250, window.innerHeight - 65, "text");
-  InstrMouse = new component("20px", "Consolas", "black", window.innerWidth - 250, window.innerHeight - 40, "text");
+  player = new BoxComponent(30, 30, "red", 10, window.innerHeight/2);
+  score = new TextComponent("SCORE: 0", "30px Consolas", "black", window.innerWidth - 200, 30);
 
   // unicodes for down and up arrows
-  UpArrSymbol = "\u2B06";
-  DownArrSymbol = "\u2B07";
+  const UpArrSymbol = "\u2B06";
+  const DownArrSymbol = "\u2B07";
 
-  InstrUp.text = "w/" + UpArrSymbol + " > up";
-  InstrDown.text = "s/" + DownArrSymbol + " > down";
-  InstrMouse.text = "m > on/off mouse control";
+  const InstrUpTxt = "w/" + UpArrSymbol + " > up";
+  const InstrDownTxt = "s/" + DownArrSymbol + " > down";
+  const InstrMouseTxt = "m > on/off mouse control";
+
+  // create a text component for each instruction box
+  InstrUp = new TextComponent(InstrUpTxt, "20px Consolas", "black", window.innerWidth - 250, window.innerHeight - 90);
+  InstrDown = new TextComponent(InstrDownTxt, "20px Consolas", "black", window.innerWidth - 250, window.innerHeight - 65);
+  InstrMouse = new TextComponent(InstrMouseTxt, "20px Consolas", "black", window.innerWidth - 250, window.innerHeight - 40);
 
   gameArea.start();
 }
 
-/* Construct a component with specific width
+/* Construct a text component with specific text
+   font, color and coordinates where it is
+   placed in the canvas */
+function TextComponent(text, font, color, x, y) {
+  this.text = text;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+      ctx = gameArea.context;
+      ctx.font = font;
+      ctx.fillStyle = color;
+      ctx.fillText(this.text, this.x, this.y);
+    }
+} // TextComponent
+
+/* Construct a box component with specific width
    height, color and coordinates where it is
    placed in the canvas */
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.width = width;
-    this.height = height;
+function BoxComponent(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.speedX = 0;
+  this.speedY = 0;
+  this.x = x;
+  this.y = y;
+  this.update = function() {
+    ctx = gameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  } // update
+  this.newPos = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (gameArea.x && gameArea.y && gameArea.mouseControl) {
+      /* if mouse position exists and mouse control 
+         is enabled set player y coord to mouse y coord
+         - player only moves vertically*/
+      this.y = gameArea.y;
+    }
+  } // newPos
+  this.newSpeed = function() {
     this.speedX = 0;
     this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.mouseControl = false;
-    this.update = function() {
-      ctx = gameArea.context;
-      if (this.type == "text") {
-        ctx.font = this.width + " " + this.height;
-        ctx.fillStyle = color;
-        ctx.fillText(this.text, this.x, this.y);
-      } else {
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-      }
-    }
-    this.newPos = function() {
-      this.x += this.speedX;
-      this.y += this.speedY;
 
-      if (gameArea.x && gameArea.y && gameArea.mouseControl) {
-        /* if mouse position exists and mouse control 
-           is enabled set player y coord to mouse y coord
-           - player only moves vertically*/
-        this.y = gameArea.y;
-      }
-    }
-    this.newSpeed = function() {
-      this.speedX = 0;
-      this.speedY = 0;
+    keyPressed = getKey(gameArea.key);
+    switch(keyPressed) {
+      case keyActions.UP:
+        this.speedY = -3;
+        break;
+      case keyActions.DOWN:
+        this.speedY = 3;
+        break;
+      default:
+        break;
+    } // switch
+  } // newSpeed
+  this.crashWith = function(otherObj) {
+    /* Get left, right, top, bottom for this object */
+    var l = this.x;
+    var r = this.x + (this.width);
+    var top = this.y;
+    var bttm = this.y + (this.height);
 
-      keyPressed = getKey(gameArea.key);
-      switch(keyPressed) {
-        case keyActions.UP:
-          this.speedY = -3;
-          break;
-        case keyActions.DOWN:
-          this.speedY = 3;
-          break;
-        default:
-          break;
-      } // switch
-    },
-    this.crashWith = function(otherObj) {
-      /* Get left, right, top, bottom for this object */
-      var l = this.x;
-      var r = this.x + (this.width);
-      var top = this.y;
-      var bttm = this.y + (this.height);
+    /* Get left, right, top, bottom for other object */
+    var otherL = otherObj.x;
+    var otherR = otherObj.x + (otherObj.width);
+    var otherTop = otherObj.y;
+    var otherBttm = otherObj.y + (otherObj.height);
 
-      /* Get left, right, top, bottom for other object */
-      var otherL = otherObj.x;
-      var otherR = otherObj.x + (otherObj.width);
-      var otherTop = otherObj.y;
-      var otherBttm = otherObj.y + (otherObj.height);
-
-      return bttm >= otherTop && top <= otherBttm && r >= otherL && l <= otherR;
-  }
-} // component
+    return bttm >= otherTop && top <= otherBttm && r >= otherL && l <= otherR;
+  } // crashWith 
+} // BoxComponent
 
 function updateGameArea() {
-
   if (crashOccurs()) {
     gameArea.stop();
     return;
@@ -204,8 +210,8 @@ function addObstacle() {
     maxGap = gameArea.canvas.height/3;
     gap = gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
 
-    obstacles.push(new component(50, height, "green", xObstacle, 0));
-    obstacles.push(new component(50, xObstacle - height - gap, "green", xObstacle, height + gap));
+    obstacles.push(new BoxComponent(50, height, "green", xObstacle, 0));
+    obstacles.push(new BoxComponent(50, xObstacle - height - gap, "green", xObstacle, height + gap));
   }
 } // addObstacle
 
