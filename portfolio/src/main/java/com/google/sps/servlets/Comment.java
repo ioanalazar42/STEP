97eb3b13f9d08e0f869f7724612da80fa8481080
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-22 09:32:14 +0000

    Change field in Comment class from text to body

--- portfolio/src/main/java/com/google/sps/servlets/Comment.java
+++ portfolio/src/main/java/com/google/sps/servlets/Comment.java
@@ -3,12 +3,12 @@ package com.google.sps.data;
 public final class Comment {
 
   private final long id;
-  private final String text;
+  private final String body;
   private final long timestamp;
 
-  public Comment(long id, String text, long timestamp) {
+  public Comment(long id, String body, long timestamp) {
     this.id = id;
-    this.text = text;
+    this.body = body;
     this.timestamp = timestamp;
   }
 }
