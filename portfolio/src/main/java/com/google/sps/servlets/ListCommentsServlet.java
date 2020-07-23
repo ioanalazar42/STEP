a621c5b2b60aaca23db0046f16c843e989d4da8f
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-23 15:04:37 +0000

    Replace break statement with return statement

--- portfolio/src/main/java/com/google/sps/servlets/ListCommentsServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ListCommentsServlet.java
@@ -47,7 +47,7 @@ public class ListCommentsServlet extends HttpServlet {
 
     int addedSoFar = 0;
     for (Entity entity : results.asIterable()) {
-      if (addedSoFar == commentLimit) { break; }
+      if (addedSoFar == commentLimit) { return; }
 
       long id = entity.getKey().getId();
       String body = (String) entity.getProperty("body");
