d8ef2adb9953f7f2231c0b7c85435de85ee587a4
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-28 09:31:59 +0000

    Fix more java linter fails

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -4,7 +4,6 @@ import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +14,8 @@ public class LoginServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     UserService userService = UserServiceFactory.getUserService();
-
     boolean loggedIn = userService.isUserLoggedIn();
-
-    String url = loggedIn ?  userService.createLogoutURL("/") : userService.createLoginURL("/");
+    String url = loggedIn ? userService.createLogoutURL("/") : userService.createLoginURL("/");
 
     response.setContentType("application/json;");
     response.getWriter().println(toJson(loggedIn, url));
@@ -27,7 +24,6 @@ public class LoginServlet extends HttpServlet {
   /* Return json string that specifies if the user is logged in and
   provides a login/logout url */
   public String toJson(boolean loggedIn, String url) {
-
     String json = "{\"logged\": ";
 
     if (loggedIn) {
