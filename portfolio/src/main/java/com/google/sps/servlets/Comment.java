fd6b7c0786f5f6484da1a7f98cc89d42d5abdd42
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-28 11:24:07 +0000

    Add sentiment analysis score field to Comment

--- portfolio/src/main/java/com/google/sps/servlets/Comment.java
+++ portfolio/src/main/java/com/google/sps/servlets/Comment.java
@@ -5,10 +5,12 @@ public final class Comment {
   private final long id;
   private final String body;
   private final long timestamp;
+  private final float score;
 
-  public Comment(long id, String body, long timestamp) {
+  public Comment(long id, String body, long timestamp, float score) {
     this.id = id;
     this.body = body;
     this.timestamp = timestamp;
+    this.score = score;
   }
 }
