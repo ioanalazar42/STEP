f8f848e0c94d8b14d34de6482c44bbfd2e4abcec
Ioana Lazar <ioanalazar459@gmail.com>
2020-07-28 16:08:25 +0000

    Add struct so that login servlet can keep track of user login status
    * Eliminate method that constructed json as it is now done automatically through gson

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -3,6 +3,7 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,31 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login-status")
 public class LoginServlet extends HttpServlet {
+  /* Struct that holds the user's login state */
+  private class UserStatus {
+    boolean logged;
+    String url;
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     UserService userService = UserServiceFactory.getUserService();
-    boolean loggedIn = userService.isUserLoggedIn();
-    String url = loggedIn ? userService.createLogoutURL("/") : userService.createLoginURL("/");
+
+    UserStatus userStatus = new UserStatus();
+    userStatus.logged = userService.isUserLoggedIn();
+    userStatus.url = userStatus.logged ? userService.createLogoutURL("/") : userService.createLoginURL("/");
+
+    Gson gson = new Gson();
 
     response.setContentType("application/json;");
-    response.getWriter().println(toJson(loggedIn, url));
-  }
-
-  /* Return json string that specifies if the user is logged in and
-  provides a login/logout url */
-  public String toJson(boolean loggedIn, String url) {
-    String json = "{\"logged\": ";
-
-    if (loggedIn) {
-      json += "true";
-    } else {
-      json += "false";
-    }
-
-    json += ", ";
-    json += "\"url\": ";
-    json += "\"" + url + "\"}";
-
-    return json;
+    response.getWriter().println(gson.toJson(userStatus));
   }
 }
